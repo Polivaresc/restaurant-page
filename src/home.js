b03a8c977fb64ff2d92a09ebc490f792a63167c7
Polivaresc <paulaolca98@gmail.com>
2022-01-20 19:17:58 +0100

    images fixed

--- src/home.js
+++ src/home.js
@@ -1,3 +1,6 @@
+import Salad from './salad.jpg'
+import Pizza from './pizza.jpg'
+
 export default function homePage() {
     const images = document.createElement('div')
     
@@ -5,10 +8,14 @@ export default function homePage() {
     const d1 = document.createElement('p')
     const d2 = document.createElement('p')
 
+    const mySalad = new Image()
+    mySalad.src = Salad
+    const myPizza = new Image()
+    myPizza.src = Pizza
+
     images.classList.add('images')
     descriptions.classList.add('descriptions')
-
-    images.innerHTML = '<img src="https://media.istockphoto.com/photos/vegetarian-dishes-picture-id1313418058?b=1&k=20&m=1313418058&s=170667a&w=0&h=-BZRud6u510emxg26hpFdOtsPSjOEsB0OCsIue_cdi8=" alt="pizza">'
+    images.append(mySalad, myPizza)
 
     d1.textContent = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis voluptatibus rem deleniti nihil temporibus velit qui error libero nulla unde assumenda delectus necessitatibus enim fugiat debitis ratione magni explicabo, ullam amet excepturi. Eius dolorum pariatur quibusdam corrupti ratione beatae atque?'
     d2.textContent = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio veniam quae asperiores, tenetur quas officia odio, totam voluptates officiis fugiat error doloremque reiciendis iure tempora deleniti, sunt fugit harum eveniet possimus ullam at aliquam nemo eum? Iure maxime fugit aut quasi, animi nisi eos, necessitatibus vel nostrum atque error fugiat obcaecati natus eum. Eveniet ratione aperiam voluptas natus architecto fugit.'
